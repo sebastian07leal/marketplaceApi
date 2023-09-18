e603e68a1a33743788889b26c8084c20a03c6a72
Sebastian Leal <sebas@192.168.10.8>
2023-09-18 14:56:40 -0500

    [CONFIGURATION] deploy vercel v1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 8080
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 8080
 
-app.get('/ping', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
